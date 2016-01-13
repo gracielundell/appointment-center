f7e6755d2f47616d7420d92168d8aa9deffed02b
Gracie Lundell <gracielundell@gmail.com>
2016-01-12 22:11:14 -0800

    Initialize js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 $(document).read(function () {
-  
+  $("#blanks form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var dateInput = $("input#date").val();
+    var startInput = $("input#start").val();
+    var endInput = $("input#end").val();
+
+    $(".name").text(nameInput);
+    $(".date").text(dateInput);
+    $(".start").text(startInput);
+    $(".end").text(endInput);
+
+    $("#hang-out").show();
+
+    event.preventDefault();
+  });
 });
